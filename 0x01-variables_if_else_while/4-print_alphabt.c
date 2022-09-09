9b61a47db4ad8cc082492f38beabf2b6a6831272
“Queen” <qunogwu@gmail.com>
2022-09-09 08:38:46 -0700

    chmod u+x 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Prints alphabet
+ *@void: Empty parameter list for main.
+ *Description: Prints the alphabetin lower case skip q and e
+ *Can only use putchar
+ *Return: 0 (Suceess)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'q')
+		{
+			letter++;
+		}
+		else if (letter == 'e')
+		{
+			letter++;
+		}
+		else
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+	putchar('\n');
+	return (0);
+
+
