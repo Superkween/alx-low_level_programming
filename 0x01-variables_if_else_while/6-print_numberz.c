cbb8eee8d012eaf12957421eb194f942cc94e4cd
“Queen” <qunogwu@gmail.com>
2022-09-09 09:03:55 -0700

    chmod u+x 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Prints alphabet
+ *@void: Empty parameter list for main.
+ *Description: Prints the alphabet in reverse order
+ *can only use putchar
+ *Return: 0 (success)
+ */
+int main(void)
+{ 
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter --;
+	}
+	putchar('\n');
+	return (0);
+}
