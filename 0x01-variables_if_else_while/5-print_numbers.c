77e36de23b77c1484a2a2e148dad0bb713bed202
“Queen” <qunogwu@gmail.com>
2022-09-09 08:51:47 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlid.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Prints base 10 numbers
+ *@void: Empty parameter list for main.
+ *Description: Prints numbers 0-9
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
