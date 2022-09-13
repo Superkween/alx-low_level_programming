0a4d6a73a7b0edcb9530e0e5d07209cc999a092e
“Queen” <qunogwu@gmail.com>
2022-09-13 06:52:58 -0700

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,2 @@
-#include "main.h"
-
-/**
- *main - Entry point
- *Return: 0 (success)
- */
-int main(void)
-{
-	_putchar;
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}
+#include
 
