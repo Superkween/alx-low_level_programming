6a819517211a4e32708cea0b2c6f07c75392a2eb
“Queen” <qunogwu@gmail.com>
2022-09-15 10:01:38 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size: size of square
+ */
+
+void print_square(int size)
+{
+	int i, j;
+	int c = 35;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (ij = 1; j <= size; j++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n);
+	}
+	if (size <= 0)
+	{
+		_putchar('\n')
+	}
+}
