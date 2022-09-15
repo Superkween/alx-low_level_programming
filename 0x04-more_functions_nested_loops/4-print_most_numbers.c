3bef6fcfc6c34828d0400f40e703fbffe2398764
“Queen” <qunogwu@gmail.com>
2022-09-15 06:54:19 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	int = 0;
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar (i + '0');
+		}
+		i++;
+	}
+
+	_putchar ('\n');
+}
+
