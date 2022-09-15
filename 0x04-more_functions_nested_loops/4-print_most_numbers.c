2fd7a7b2659147a22aaa6c69c84fbe5bc9e5dfec
“Queen” <qunogwu@gmail.com>
2022-09-15 06:59:46 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,16 +8,13 @@ void print_most_numbers(void)
 {
 	int i;
 
-	int = 0;
-	while (i < 10)
+	for (i = 48; i <= 57; i++)
 	{
-		if (i != 2 && i != 4)
+		if (i != 50 && i != 52)
 		{
-			_putchar (i + '0');
+			_putchar (i);
 		}
-		i++;
 	}
 
 	_putchar ('\n');
 }
-
