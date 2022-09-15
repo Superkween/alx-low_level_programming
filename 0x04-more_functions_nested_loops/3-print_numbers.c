d19a0ae74312d4e2a1e461116cf586408c9e0ad8
“Queen” <qunogwu@gmail.com>
2022-09-15 06:33:29 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers.
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	int = 0;
+	while (i < 10)
+	{
+		_putchar (i + '0')
+			i++;
+	}
+
+	_putchar ('\n');
+}
