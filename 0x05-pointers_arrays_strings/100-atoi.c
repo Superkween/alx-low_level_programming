ad8fd554e7fff5b06f565b413d859eb66adc5bc8
“Queen” <qunogwu@gmail.com>
2022-09-19 09:53:47 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -83,7 +83,7 @@ int _atoi(char *s)
 	while ((s[idx_digit_starts] >= '0' && s[idx_digit_starts] <= '9')
 			&& (idx_digit_starts <= _strlen(s))) /* count digits to print */
 	{
-			digits_to_prints += 1;
+			digits_to_print += 1;
 			idx_digit_starts++;
 	}
 
