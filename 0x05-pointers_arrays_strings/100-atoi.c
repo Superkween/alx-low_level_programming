67d8ef64d61ecf8107d9776b30d71ae162a9d016
“Queen” <qunogwu@gmail.com>
2022-09-19 13:18:00 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,104 +1,42 @@
 #include "main.h"
 
-
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
- * idx_num_starts - find index where a digit is first found in string
- * @s: string to search
- * Return: integer index where digit is first found
- */
-
-int idx_num_starts(char *s)
-{
-	int i;
-
-	for (i = 0; i < _strlen(s); i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-			return (i);
-	}
-	return (-1); /* return -1 if no digits found */
-}
-
-/**
- * find_sign - determine if integer is negative
- * @s: integer
- * Return: integer 1 or -1
- */
-int find_sign(char *s)
-{
-	int negatives = 0, i = 0, sign = 1;
-
-	while (i < (idx_num_starts(s)))
-	{
-		if (s[i++] == '-')
-			negatives++;
-	}
-
-	if (negatives % 2 != 0)
-		sign = -1;
-
-	return (sign);
-}
-
-/**
- * _atoi - convert string to int
- * @s: string to convert
- * Return: integer
+ * _atoi - Convert a string to integer.
+ * @s: char array string
+ * Description: Number in the string can be preceded by an infinite
+ * number of characters.
+ * You need to take into account all -/+ signs before the number
+ * If there are no numbers in the string, return 0.
+ * No need to check for overflow.
+ * Not allowed to hard-code special values.
+ * Return: first integer found in string
  */
 
 int _atoi(char *s)
 {
+	int i;
+	int h, p;
 
-	int idx_digit_starts = (idx_num_starts(s));
-	int sign;
-	int digits_to_print = 0;
-	int t = 1, i;
-	unsigned int num = 0;
-	int digit = (idx_num_starts(s));
-
-	if (idx_digit_starts < 0) /* if no digits found, exit program */
-		return (0);
-
-	sign = find_sign(s);
-
-	while ((s[idx_digit_starts] >= '0' && s[idx_digit_starts] <= '9')
-			&& (idx_digit_starts <= _strlen(s))) /* count digits to prints */
+	h = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-			digits_to_print += 1;
-			idx_digit_starts++;
-	}
+		if (s[i] == '-')
+			p *= -1;
 
-	i = 1;
-	while (i < digits_to_print) /* find powers of ten to multiply places */
-	{
-		t *= 10;
-		i++;
-	}
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
+			else
+				h = (s[i] - '0') * -1;
 
-	for (i = digit; i < (digit + digits_to_print); i++) /* calculate num */
-	{
-		num += (s[i] - '0') * t;
-		t /= 10;
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
 	}
+	if (p < 0)
+		h *= -1;
 
-	return (num * sign);
+	return (h);
 }
