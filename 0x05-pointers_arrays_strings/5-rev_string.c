a156900193e763f581562530842be1af4cc824a4
“Queen” <qunogwu@gmail.com>
2022-09-19 08:14:55 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,34 +7,3 @@
  */
 
 int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
- * rev_string - reverse a string
- * @s: string to reverse
- */
-
-void rev_string(cahr *s)
-{
-	int i = 0;
-	int j = (_strlens(s) - 1);
-	char tmp;
-
-	while (i < j)
-	{
-		tmp = s[i];
-		s[i] = s[j];
-		s[j] = tmp;
-		i++, j--;
-	}
-}
