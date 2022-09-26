fc488ea12107cb9452bfc8c0c3d712876f9acab8
“Queen” <qunogwu@gmail.com>
2022-09-26 11:27:45 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,11 +2,9 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: content is to be copied
- * @to: string
- * Return: void/
+ * @s: pointer to pointer we need to set to
+ * @to: string to set
  */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
