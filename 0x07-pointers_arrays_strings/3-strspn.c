511b7c353eda1abfb458b5173f8edc594ec5c294
“Queen” <qunogwu@gmail.com>
2022-09-26 09:45:53 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,29 +2,48 @@
 
 /**
  * _strchr - Locates a character in a string
- * @s: String
- * @c: Character to search
+ * @s: this is the main string to be scanned.
+ * @accept: this is the string containing the list of characters to match in s
  *
- * Return: Pointer to the first occurrence of the character c in the string s
+ * Return: return count
  */
 
-char *_strchr(char *s, char c)
+unsigned int _strspn(char *s, char *accept)
 {
-	while (*s != '\0') /*Declaring WHILE*/
+	int i, j;
+
+	int night = 0;
+
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+
+	i = 0;
+
+	while (str1[i] != '\0')
 	{
-		if (*s == c) /*if s ==  c*/
+		j = 0;
+
+
+		while (str2[j] != '\0')
 		{
-			return (s); /*return s*/
+			if (str2[j] == str1[i])
+			{
+				night++;
+				break;
+
+			}
+			j++;
 		}
+		if (s[i] != accept[j])
 
-		++s;
+		{
+			break;
+
+		}
+		i++;
 	}
+	return (night); /*return the value of night*/
 
-	if (*s == c)
-	{
-		return (s);
-	}
-
-
-	return (0);/* values null*/
 }
